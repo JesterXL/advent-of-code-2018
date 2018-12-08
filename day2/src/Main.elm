de611a8935064713c5dd584f10ebc01995d5df33
Jesse Warden <jesterxl@jessewarden.com>
2018-12-08 17:26:50 -0500

    attempt 2 failure number 2, heh. I'm close I think

--- day2/src/Main.elm
+++ day2/src/Main.elm
@@ -4,9 +4,13 @@ import Browser
 import Debug exposing (log)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-import List exposing (foldl, length, partition, map, filter)
+import List exposing (foldl, length, partition, map, filter, head, reverse)
 import String exposing (split, toList)
-import Tuple exposing (first)
+import Tuple exposing (first, pair)
+import Dict exposing (Dict)
+import Maybe exposing (withDefault)
+import Set exposing (Set, insert, empty)
+import Char exposing (toCode, fromCode)
 
 
 type alias Model =
@@ -69,13 +73,33 @@ reduceCharacterMatch acc has2LettersSame has3LettersSame =
     else
         acc
 
+last : List a -> Maybe a
+last list =
+    head (reverse list)
+
+
+
+getDefultCharacter : List Char -> Char
+getDefultCharacter list =
+    Maybe.withDefault '?' (List.head list)
 
 -- [['n'],['v','v'],['o'],['s'],['v','v'],['k'],['c'],['i','i'],['t'],['d'],['b'],['f'],['i','i'],['y'],['x'],['u'],['p'],['h'],['z'],['g'],['e'],['r','r'],['a'],['r','r'],['j'],['q']]
 -- [['v','v'],['v','v'],['i','i'],['i','i'],['r','r'],['r','r']]
-checkPartitionLol charList =
+partitionTwosAndThress charList =
     map (\char -> partition (\letter -> letter == char) charList) charList
     |> map (\item -> first item)
     |> filter (\list -> length list == 2 || length list == 3)
+    -- |> partition (\list -> length list == 2)
+    -- |> headCharList
+    -- |> Maybe.withDefault []
+    -- |> reduceDuplicateLettersToSet
+
+partitionTwos list =
+    partition (\chars -> length chars == 2) list     
+
+partitionThrees list =
+    partition (\chars -> length chars == 3) list
+        
     -- |> map length
     -- |> filter (\item -> item == 2 || item == 3)
     -- |> foldl (\item acc ->
@@ -86,6 +110,7 @@ checkPartitionLol charList =
     --             else
     --                 acc) (CharacterMatch 0 0)
 
+
 reduceCharListToCharacterMatches charList item acc =
     let
         has2LettersSame =
@@ -95,8 +120,16 @@ reduceCharListToCharacterMatches charList item acc =
             has3LettersThatAreTheSame charList item
 
         demLetters = 
-            checkPartitionLol charList
-        msg1 = log "demLetters" demLetters
+            partitionTwosAndThress charList
+        -- msg1 = log "demLetters" demLetters
+        the2 = log "the2" (partitionTwos demLetters)
+        -- the3 = log "the3" (partitionThrees demLetters)
+        -- twos = partitionTwos demLetters
+        -- twosMapped = List.map (\i -> List.head i) twos
+        -- m3 = log "m3" twosMapped
+        -- that = foldl (\item2 acc2 ->
+        --     pair (withDefault '?' (head item2) ) (withDefault '?' (last item2) ) :: acc2) [] demLetters
+        -- msg3 = log "that" that
         -- msg2 = log "charList" charList
     in
     reduceCharacterMatch acc has2LettersSame has3LettersSame
@@ -121,18 +154,96 @@ calculateChecksum : CharacterMatch -> Int
 calculateChecksum characterMatch =
     characterMatch.char2 * characterMatch.char3
 
+setInsertRight : Set Char -> Char -> Set Char
+setInsertRight set value =
+    Set.insert value set
+
+insertWithDefault : List Char -> Set Char -> Set Char
+insertWithDefault listWithLetters letterSet =
+    List.head listWithLetters
+    |> Maybe.withDefault '?'
+    |> setInsertRight letterSet
+
+reduceDuplicateLettersToSet : List (List Char) -> Set Char
+reduceDuplicateLettersToSet list =
+    List.foldl insertWithDefault Set.empty list
+
+partitionSameLetters charList letter =
+    partition (\char -> char == letter) charList
+    |> Tuple.first
+
+-- has2LettersThatAreTheSame
+filterOutSameLetters charList =
+    List.map (partitionSameLetters charList) charList
+    |> List.filter (\matchList -> List.length matchList > 1)
+
+sortCharsAlphabetical : List Char -> List Char
+sortCharsAlphabetical list =
+    List.map Char.toCode list
+    |> List.sort
+    |> List.map Char.fromCode
+
+-- condenseSameCharMatchesToSingles : List Char -> List Char
+
+getFirstLetterOrNoClue : List Char -> Char
+getFirstLetterOrNoClue list =
+    Maybe.withDefault '?' (List.head list)
+
+incrementDictValue : Dict Char Int -> Char -> Dict Char Int
+incrementDictValue dict key =
+    let
+        currentValue = Maybe.withDefault 0 (Dict.get key dict)
+    in
+        Dict.update key (\maybeVal -> Just (currentValue + 1)) dict
+
+count2And3CharactersInChecksum : List Char -> CharacterMatch
+count2And3CharactersInChecksum checksumCharList =
+    filterOutSameLetters checksumCharList
+    |> List.foldl (\matchList foundDict -> incrementDictValue foundDict (getFirstLetterOrNoClue matchList)) Dict.empty
+    |> Dict.foldl (\charKey charCount charMatch ->
+        if charCount == 2 then
+            { charMatch | char2 = charMatch.char2 + 1}
+        else if charCount == 3 then
+            { charMatch | char3 = charMatch.char3 + 1}
+        else
+            charMatch) (CharacterMatch 0 0)
+
+combineCharacterMatches : List CharacterMatch -> CharacterMatch
+combineCharacterMatches characterMatches =
+    List.foldl (\match acc -> { acc | char2 = acc.char2 + match.char2, char3 = match.char3 }) (CharacterMatch 0 0) characterMatches
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
             let
-                -- Attempt #1: The checksum 85272 is too damn high
-                checksum = 
+                -- Attempt #1: The checksum 85272 is too damn high!
+                -- Attempt #2: The checksum 646 is too low, like my self-esteem in this language.
+                -- charTotal = 
+                --     List.take 10 (split "\n" checksums)
+                --     |> List.map toList
+                --     |> List.map sortCharsAlphabetical
+                --     |> List.head
+                --     |> Maybe.withDefault []
+                --     |> filterOutSameLetters
+                --     -- |> List.foldl (\matchList foundSet -> Set.insert (getFirstLetterOrNoClue matchList) foundSet) Set.empty
+                --     |> List.foldl (\matchList foundDict -> incrementDictValue foundDict (getFirstLetterOrNoClue matchList)) Dict.empty
+                --     |> Dict.foldl (\charKey charCount charMatch ->
+                --         if charCount == 2 then
+                --             { charMatch | char2 = charMatch.char2 + 1}
+                --         else if charCount == 3 then
+                --             { charMatch | char3 = charMatch.char3 + 1}
+                --         else
+                --             charMatch) (CharacterMatch 0 0)
+                charTotal =
+                    -- List.take 10 (split "\n" checksums)
                     split "\n" checksums
-                    |> map findDuplicateCharacters
-                    |> foldl reduceCharacterMatches (CharacterMatch 0 0)
-                    |> calculateChecksum
-                um = log "checksum" checksum
+                    |> List.map toList
+                    |> List.map sortCharsAlphabetical
+                    |> List.map count2And3CharactersInChecksum
+                    |> combineCharacterMatches
+
+                um = log "charTotal" charTotal
             in
             { model | count = model.count + 1 }
 
