dcf1af2da8aff8d87fb38ec7ae6dd1c087d720ff
Jesse Warden <jesterxl@jessewarden.com>
2018-12-25 23:13:37 -0500

    initial commit

---
+++ day5/src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// require('./compare')
+require('./index.html');
+// require('./elm-canvas.js')
+const Elm = require('./Main.elm')
+// const { reduce } = require('lodash/fp')
+
+// Elm.Main.embed(document.getElementById('main'));
+const app = Elm.Elm.Main.init({ node: document.querySelector('#main') })
