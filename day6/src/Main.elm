ee2bbbf4836faf2b8e2512a82de2f2698cdf771a
Jesse Warden <jesterxl@jessewarden.com>
2018-12-29 14:32:38 -0500

    super close, fixed a bug in manhattan distance; parenthesis are SUUUPER important to order of operations in Elm, you can get bad results with no warnings, heh! I need to remove the equal distance dots and I think I'll have a more accurate picture

--- day6/src/Main.elm
+++ day6/src/Main.elm
@@ -31,12 +31,14 @@ import CanvasColor as Color exposing (Color)
 type alias Model =
     { coordinatesText : String
     , coordinates : List Coordinate
+    , tiles : List Coordinate
     }
 
 initialModel : Model
 initialModel =
     { coordinatesText = ""
     , coordinates = []
+    , tiles = []
     }
 
 type Msg
@@ -107,13 +109,23 @@ getManhattanDistance : Point -> Point -> Int
 getManhattanDistance point1 point2 =
     let
         size = 1 -- I have a feeling I may regret this later, but it's a pain passing it down
-        x1 = abs point1.x - point2.x
-        x2 = abs point2.x - point1.x
+        x1 = abs (point1.x - point2.x)
+        x2 = abs (point2.x - point1.x)
         dx = min x1 (size - x2)
-        y1 = abs point1.y - point2.y
-        y2 = abs point2.y - point1.y
+        y1 = abs (point1.y - point2.y)
+        y2 = abs (point2.y - point1.y)
         dy = min y1 (size - y2)
         distance = abs (dx + dy)
+
+        -- point1log = log "point1" point1
+        -- point2log = log "point2" point2
+        -- x1log = log "x1" x1
+        -- x2log = log "x2" x2
+        -- dxlog = log "dx" dx
+        -- y1log = log "y1" y1
+        -- y2log = log "y2" y2
+        -- dylog = log "dy" dy
+        -- distancelog = log "distance" distance
     in
         distance
 
@@ -134,28 +146,85 @@ getDistanceBetweenCoordinates : Coordinate -> Coordinate -> Int
 getDistanceBetweenCoordinates coordinate1 coordinate2 =
     getManhattanDistance coordinate1.point coordinate2.point
 
+-- TODO: I think modifying this function to return coord instead of coordinate gives you the
+-- distance too the target coordinate, but it removes "who" the original coordinate is. You need both.
+-- we should probably return a Dictionary instead of a Tuple.
 getShortestDistanceFromAllOtherCoordinates : Coordinate -> Array Coordinate -> (Int, Coordinate)
 getShortestDistanceFromAllOtherCoordinates coordinate coordinates =
     Array.foldl (\coord acc->
         let
+
             distance = getDistanceBetweenCoordinates coordinate coord
             currentShortestDistance = Tuple.first acc
             -- d1 = log "diff" ("distance: " ++ String.fromInt distance ++ ", currentShortestDistance: " ++ String.fromInt currentShortestDistance)
             targetCoordinate = Tuple.second acc
         in
+            -- TODO: need to figure out how to do dots, this doesn't appear to be working
             if distance == currentShortestDistance then
                 -- we've got a dodson here
-                (-1, { coordinate | label = "." })
+                (-1, coord)
             else if distance < currentShortestDistance then
                 (distance, coord)
             else
                 acc) (20000, coordinate) coordinates
 
-calculateCoordinateDistanceForRow : Array Coordinate -> Array Coordinate -> Array String
+-- NOTE: Return value is a bit complex and I keep forgetting so...
+-- coordinate: Coordinate in the row. 0, 0 == coordinate in that particular mdarray.
+-- distance: how far is the coordinate from the targetCoordinate. -1 means 0 distance. -2 means it's on the edge and infinite.
+-- targetCoordinate: Target Coordinate is the big ole list of coordinates we've parsed from our puzzle input.
+-- calculateCoordinateDistanceForRow : Array Coordinate -> Array Coordinate -> Array String
 calculateCoordinateDistanceForRow row targetCoordinates =
-    Array.map (\targetCoordinate -> getShortestDistanceFromAllOtherCoordinates targetCoordinate targetCoordinates) row
-    |> Array.map Tuple.second
-    |> Array.map .label
+    Array.map (\targetCoordinate -> (targetCoordinate, getShortestDistanceFromAllOtherCoordinates targetCoordinate targetCoordinates)) row
+    -- |> Array.slice 0 600
+    |> Array.map (\tuples ->
+        let
+            coordinate = Tuple.first tuples
+            distanceAndTarget = Tuple.second tuples
+            target = Tuple.second distanceAndTarget
+            datEdge = coordinateTouchesEdge 600 600 coordinate
+            -- log1 = log "datEdge" datEdge
+        in
+        if datEdge == True then -- moarrrr hardcoding regrets
+            (coordinate, (-2, target))
+        else
+            tuples)
+
+updateCountedCoordinateDictionary : String -> Dict String Int -> Dict String Int
+updateCountedCoordinateDictionary label dict =
+    Dict.update label (\countMaybe ->
+        case countMaybe of
+            Nothing ->
+                Just 1
+            Just val ->
+                Just (val + 1)) dict
+
+
+coordinateTouchesEdge : Int -> Int -> Coordinate -> Bool
+coordinateTouchesEdge rowCount columnCount coordinate =
+    if coordinate.point.x == 0 then
+        True
+    else if coordinate.point.x == (columnCount - 1) then
+        True
+    else if coordinate.point.y == 0 then
+        True
+    else if coordinate.point.y == (rowCount - 1) then
+        True
+    else
+        False
+
+getInfiniteSet : Array (Coordinate, (Int, Coordinate)) -> Set String
+getInfiniteSet array =
+    Array.foldl (\item acc ->
+        let
+            coordinate = Tuple.first item
+            distanceAndTarget = Tuple.second item
+            distance = Tuple.first distanceAndTarget
+            target = Tuple.second distanceAndTarget
+        in
+        if distance == -2 then
+            Set.insert target.label acc
+        else
+        acc) Set.empty array
 
 update : Msg -> Model -> Model
 update msg model =
@@ -166,7 +235,7 @@ update msg model =
                 rows = getRows
             
                 mdarray =
-                    getMDArray rows cols
+                    getMDArray cols rows
                     -- |>    
                 -- mdarraylog = log "mdarray" mdarray
 
@@ -185,19 +254,73 @@ update msg model =
 
                 coordinatesArray =
                     Array.fromList model.coordinates
-                um =
+                
+                coordinateAndDistanceTargets =
                     Array.map (\row -> calculateCoordinateDistanceForRow row coordinatesArray) mdarray
-                umlog = log "um" um
+                    |> Array.foldl (\row acc ->
+                        Array.foldl (\tuple deepAcc -> Array.push tuple deepAcc) acc row) Array.empty
+                        
+                -- NOTE: I give up on infinite for now, lol, this is hard... it keeps getting
+                -- every single tile as somehow having at least 1 on the edge. This means
+                -- that technically every area is infinite which can't be correct, 
+                -- else my puzzle input is bugged like they said Day 6 had a bug.
+                -- I'm betting on my crappy math skills being the problem.
 
+                -- infiniteLabels =
+                --     getInfiniteSet coordinateAndDistanceTargets
+                -- infiniteLabelslog = log "infiniteLabels" infiniteLabels
+                -- finiteDistanceTargets =
+                --     Array.filter (\item ->
+                --         let
+                --             coordinate = Tuple.first item
+                --             target = Tuple.second (Tuple.second item)
+                --             -- log1 = log "datmember" (Set.member target.label infiniteLabels)
+                --         in
+                --             if Set.member target.label infiniteLabels then
+                --                 False
+                --             else
+                --                 True) coordinateAndDistanceTargets
+                --     |> Array.length
+
+                -- umlog = log "coordinateAndDistanceTargets" coordinateAndDistanceTargets
+                -- finiteDistanceTargetslog = log "finiteDistanceTargets" finiteDistanceTargets
+                
+                counted = 
+                    Dict.empty
+                
+                total =
+                    coordinateAndDistanceTargets
+                    |> Array.foldl (\tuples totalSet ->
+                        let
+                            closestTarget = Tuple.second (Tuple.second tuples)
+                        in
+                        updateCountedCoordinateDictionary closestTarget.label totalSet) counted
+                    |> Dict.toList
+                    |> List.sortBy Tuple.second
+                -- totallog = log "total" total
+
+                tiles =
+                    coordinateAndDistanceTargets
+                    |> Array.foldl (\tuples acc ->
+                        let
+                            coordinate = Tuple.first tuples
+                            target = Tuple.second (Tuple.second tuples)
+                        in
+                         List.append [{coordinate | label = target.label}] acc) []
+                -- tileslog = log "tiles" tiles
             in
-            model
+            { model | tiles = tiles }
             
         ParseCoordinatesText ->
             let
-                -- ** Challenge 1 **
                 coordinates =
                     parseCoordinates model.coordinatesText
                 -- coordinatesLog =log "coordinates" coordinates
+
+                -- coord1 = Coordinate (Point 233 472) (PointFloat (toFloat 233) (toFloat 472)) "a"
+                -- coord2 = Coordinate (Point 124 562) (PointFloat (toFloat 124) (toFloat 562)) "b"
+                -- dist1 = getDistanceBetweenCoordinates coord1 coord2
+                -- log1 = log "dist1" dist1 // 19, now 197, w00t w00000t
             in
             { model | coordinates = coordinates }
             -- model
@@ -258,10 +381,105 @@ renderBackground width height cmds =
 drawCoordinate : Coordinate -> Commands -> Commands
 drawCoordinate coordinate cmds =
     cmds
-    |> Canvas.fillStyle canvasCoordinateDotColor
+    |> Canvas.fillStyle (Color.rgb 0 0 0)
     -- |> Canvas.fillCircle coordinate.pointFloat.x coordinate.pointFloat.y 4
-        |> Canvas.font "14px Helvetica"
-        |> Canvas.fillText coordinate.label coordinate.pointFloat.x coordinate.pointFloat.y Nothing
+    |> Canvas.fillRect coordinate.pointFloat.x (coordinate.pointFloat.y - 12) 20 14
+    |> Canvas.fillStyle canvasCoordinateDotColor
+    |> Canvas.font "14px Helvetica"
+    |> Canvas.fillText coordinate.label coordinate.pointFloat.x coordinate.pointFloat.y Nothing
+
+plotColors =
+    Array.fromList [ Color.rgb 239 83 80
+    , Color.rgb 236 64 122
+    , Color.rgb 171 71 188
+    , Color.rgb 126 87 194
+    , Color.rgb 92 107 192
+    , Color.rgb 66 165 245
+    , Color.rgb 41 182 246
+    , Color.rgb 38 198 218
+    , Color.rgb 38 166 154
+    , Color.rgb 102 187 106
+    , Color.rgb 156 204 101
+    , Color.rgb 212 225 87
+    , Color.rgb 255 238 88
+    , Color.rgb 255 202 40
+    , Color.rgb 255 167 38
+    , Color.rgb 255 112 67
+    , Color.rgb 141 110 99
+    , Color.rgb 189 189 189
+    , Color.rgb 120 144 156
+    , Color.rgb 198 40 40
+    , Color.rgb 173 20 87
+    , Color.rgb 106 27 154
+    , Color.rgb 69 39 160
+    , Color.rgb 40 53 147
+    , Color.rgb 21 101 192
+    , Color.rgb 2 119 189
+    , Color.rgb 0 131 143
+    , Color.rgb 0 105 92
+    , Color.rgb 46 125 50
+    , Color.rgb 85 139 47
+    , Color.rgb 158 157 36
+    , Color.rgb 249 168 37
+    , Color.rgb 255 143 0
+    , Color.rgb 239 108 0
+    , Color.rgb 216 67 21
+    , Color.rgb 78 52 46
+    , Color.rgb 66 66 66
+    , Color.rgb 55 71 79
+    , Color.rgb 255 23 68
+    , Color.rgb 245 0 87
+    , Color.rgb 213 0 249
+    , Color.rgb 101 31 255
+    , Color.rgb 61 90 254
+    , Color.rgb 41 121 255
+    , Color.rgb 0 176 255
+    , Color.rgb 0 229 255
+    , Color.rgb 29 233 182
+    , Color.rgb 0 230 118
+    , Color.rgb 118 255 3
+    , Color.rgb 198 255 0
+    , Color.rgb 255 234 0
+    , Color.rgb 255 196 0
+    , Color.rgb 255 145 0
+    , Color.rgb 255 61 0
+    , Color.rgb 62 39 35
+    , Color.rgb 33 33 33
+    , Color.rgb 38 50 56 ]
+
+drawPlot : Coordinate -> Color -> Commands -> Commands
+drawPlot coordinate color cmds =
+    cmds
+    |> Canvas.fillStyle color
+    |> Canvas.fillRect coordinate.pointFloat.x coordinate.pointFloat.y 1 1
+
+
+getColorElseWhite : String -> Dict String Color -> Color
+getColorElseWhite key colors =
+    Dict.get key colors
+    |> Maybe.withDefault (Color.rgb 255 255 255)
+
+drawTile : Coordinate -> Dict String Color -> Commands -> Commands
+drawTile tile labelColorDict cmds =
+    cmds
+    |> drawPlot tile (getColorElseWhite tile.label labelColorDict)
+    -- |> drawPlot tile (Color.rgb 255 255 255)
+
+drawTiles : List Coordinate -> Commands -> Commands
+drawTiles tiles cmds =
+    let
+        colors = plotColors
+        labelColorDict =
+            Array.indexedMap (\index label ->
+                let
+                    color = Array.get index colors |> Maybe.withDefault (Color.rgb 0 0 0)
+                in
+                    (label, color)) getCoordinateLabels
+            |> Array.toList
+            |> Dict.fromList
+    in
+        List.foldl (\tile acc -> drawTile tile labelColorDict acc) cmds tiles
+
 
 view : Model -> Html Msg
 view model =
@@ -290,12 +508,12 @@ view model =
                         Canvas.element
                             getCols
                             getRows
-                            [ style "border" "1px solid black", style "width" "400px"]
+                            [ style "border" "1px solid black", style "width" "600px"]
                             ( Canvas.empty
                                 |> Canvas.clearRect 0 0 getColsFloat getRowsFloat
                                 |> renderBackground getColsFloat getRowsFloat
+                                |> (\cmds -> drawTiles model.tiles cmds)
                                 |> (\cmds -> List.foldl drawCoordinate cmds model.coordinates)
-                                -- |> Canvas.fillText "Hello world" 50 100 Nothing
                             )
                         ]
                 ]   
---
+++ day6/src/sandbox.js
@@ -0,0 +1,42 @@
+
+// getManhattanDistance : Point -> Point -> Int
+// getManhattanDistance point1 point2 =
+//     let
+//         size = 1 -- I have a feeling I may regret this later, but it's a pain passing it down
+//         x1 = abs point1.x - point2.x
+//         x2 = abs point2.x - point1.x
+//         dx = min x1 (size - x2)
+//         y1 = abs point1.y - point2.y
+//         y2 = abs point2.y - point1.y
+//         dy = min y1 (size - y2)
+//         distance = abs (dx + dy)
+//         dxlog = log "dx" dx
+//         dylog = log "dy" dy
+//         distancelog = log "distance" distance
+//     in
+//         distance
+
+
+
+const log = console.log
+
+const dist = point1 => point2 => {
+    const size = 1
+    const x1 = Math.abs(point1.x - point2.x)
+    const x2 = Math.abs(point2.x - point1.x)
+    const dx = Math.min(x1, (size - x2))
+    const y1 = Math.abs(point1.y- point2.y)
+    const y2 = Math.abs(point2.y - point1.y)
+    const dy = Math.min(y1, (size - y2))
+    log("x1:", x1)
+    log("x2:", x2)
+    log("dx:", dx)
+    log("y1:", y1)
+    log("y2:", y2)
+    log("dy:", dy)
+    const distance = Math.abs(dx + dy)
+    return distance
+}
+// log(dist({x: 0, y: 0})({x: 4, y: 4}))
+log(dist({x: 233, y: 472})({x: 124, y: 562}))
+// 684
